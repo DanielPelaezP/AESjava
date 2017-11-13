d2b00302c87b63b34e8677facdc0d5ef19644492
DanielPelaezP <pelaez.1216@gmail.com>
2017-11-13 16:52:39 -0500

    arreglos

--- src/aes/Encriptacion.java
+++ src/aes/Encriptacion.java
@@ -16,7 +16,7 @@ import javax.crypto.spec.SecretKeySpec;
 public class Encriptacion {
     
     private final static String tipo_alg = "AES";
-    private final static String cI = "AES/CBC/PKCS5Paddig";
+    private final static String cI = "AES/CBC/PKCS5padding";
     
     public static String encrypt(String key, String vector, String Cadenaorginal) throws Exception{
     Cipher cipher = Cipher.getInstance(cI);
@@ -24,14 +24,14 @@ public class Encriptacion {
     IvParameterSpec ivParameterSpec = new IvParameterSpec(vector.getBytes());
     cipher.init(Cipher.ENCRYPT_MODE,skeySpec,ivParameterSpec );
     byte[] encrypted = cipher.doFinal(Cadenaorginal.getBytes());
-    return new String(encrypted);
+     return new String(encrypted, "ISO-8859-1");
     }
     
     public static String decrypt (String key, String vector, String Cadenacifrada) throws Exception{
         Cipher cipher = Cipher.getInstance(cI);
         SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(), tipo_alg);
         IvParameterSpec ivParameterSpec = new IvParameterSpec(vector.getBytes());
-        byte[] enc = Cadenacifrada.getBytes();
+        byte[] enc = Cadenacifrada.getBytes("ISO-8859-1");
         cipher.init(Cipher.DECRYPT_MODE,skeySpec,ivParameterSpec );
         byte[] decrypted = cipher.doFinal(enc);       
         return new String (decrypted);
--- src/aes/main.java
+++ src/aes/main.java
@@ -17,9 +17,9 @@ public class main {
         String iv = "0123456789ABCDEF";
         
         String cleartext ="hola mundo";
-        System.out.println("Texto encriptado"+ aes.Encriptacion.encrypt(key, iv, cleartext));
-        String encriptado =   aes.Encriptacion.encrypt(key, iv, cleartext);
-        System.out.println("Textto encriptado"+ aes.Encriptacion.decrypt(key, iv, encriptado));
+        System.out.println("Texto encriptado es " + aes.Encriptacion.encrypt(key, iv, cleartext));
+        String encriptado = aes.Encriptacion.encrypt(key, iv, cleartext);
+        System.out.println("Textto desencriptado " + aes.Encriptacion.decrypt(key, iv, encriptado));
     
     }
      
